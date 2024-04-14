3062cb2eb7efcf73219b2cac744e4fb915397470
Priyakodam <priya.kodam1997@gmail.com>
2024-04-14 14:40:14 +0530

    Priya

--- src/components/pages/services/Services.js
+++ src/components/pages/services/Services.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Services.css'; // Ensure the CSS file is in the same directory
+import './Services.css'; 
 import repairImage1 from '../../img/part6.jpg';
 import repairImage2 from '../../img/data1.jpg';
 import repairImage3 from '../../img/licenses5.png';
@@ -16,6 +16,7 @@ const Services = () => {
       <div className="service-container container1">
         <div className="service-image">
           <img src={repairImage1} alt="Laptop Repair" />
+          <img src={repairImage1} alt="Data Backup & Recovery" className='ipad-class mt-2'/>
         </div>
         <div className="service-content">
           <h1>Laptop Repair Service and Sales</h1>
@@ -28,10 +29,11 @@ At IT-ZONE, we understand the importance of your laptop in today's digital world
       <div className="service-container container2">
         <div className="service-content">
           <h1>Data Backup & Recovery</h1>
-          <p>Our Data Backup and Recovery services ensure your data's safety and accessibility, minimizing potential losses due to data issues.</p>
+          <p>The backup and recovery of data is the process of backing up your data in the event of a loss and setting up secure systems that allow you to recover your data as a result. Data backup requires the copying and archiving of computer data to make it accessible in case of data corruption or deletion.</p>
         </div>
         <div className="service-image">
           <img src={repairImage2} alt="Data Backup & Recovery" />
+         
         </div>
       </div>
 
@@ -41,17 +43,25 @@ At IT-ZONE, we understand the importance of your laptop in today's digital world
         </div>
         <div className="service-content">
           <h1>Software Licensing Service</h1>
-          <p>We provide a comprehensive one-stop-shop for all your software licensing needs, partnering with global IT companies to deliver top-quality products.</p>
+          <p style={{marginLeft:"2px"}}>We are a One Stop Shop for purchasing Licenses for multiple Software Products. To offer you the best software solutions, we have partnered with Global IT companies who deliver world-class software products.</p>
         </div>
       </div>
 
       <div className="service-container container4">
         <div className="service-content">
           <h2>CCTV Camera Installation</h2>
-          <p>From commercial to residential setups, ITZ-ONE offers extensive CCTV installation services to ensure your security, accessible from anywhere in the world.</p>
+          <p style={{marginLeft:"12px"}}>ITZ-ONE is a seller of security cameras, video recorders (DVR, NVR), surveillance systems, and all other things CCTV. We customize CCTV Projects on any Small or Larger number of Cameras..!
+
+All of our systems come fully equipped to handle all of your remote viewing needs. No matter where you are in the world, as long as you have an internet connection, you’ll be able to view your video feed at any time you want. You don’t need to have expert knowledge of CCTV systems, just a helpful and willing support staff that’s more than happy to assist you with anything you may need.
+
+CCTV Surveillance Projects we take up Commercial, Govt/Private, Residential, Small business or Shops.
+
+Stay “anywhere”, View from “anywhere” – free mobile app view</p>
         </div>
         <div className="service-image">
           <img src={repairImage4} alt="CCTV Camera Installation" />
+          <img src={repairImage1} alt="Data Backup & Recovery" className='ipad-class mt-2'/>
+          <img src={repairImage1} alt="Data Backup & Recovery" className='ipad-class mt-2'/>
         </div>
       </div>
     </div>
